3c147470446e587e4c3fa069cb92b9c3fe3e5900
Durzdor <maxiperez024@hotmail.com>
2021-04-22 22:33:50 -0300

    Main menu added
    +Main menu scene
    +Main menu script
    +TextMeshPro Essentials

---
+++ Progra2/Assets/Scripts/ManuScripts/MainMenuManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ButtonSwitch
+{
+    PlayButton,
+    HelpButton,
+    CreditsButton,
+    QuitButton
+}
+public class MainMenuManager : MonoBehaviour
+{
+    public List<Button> buttons;
+    public RectTransform selectorObject;
+    public GameObject mainWindow;
+    public GameObject helpWindow;
+    public GameObject creditsWindow;
+    public Button goBackButton;
+    private Button currentButton;
+    private int buttonIndex;
+
+    private void Start()
+    {
+        buttonIndex = 0;
+        currentButton = buttons[0];
+        mainWindow.SetActive(true);
+        helpWindow.SetActive(false);
+        creditsWindow.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (mainWindow.activeSelf)
+            {
+                buttonIndex--;
+                if (buttonIndex < 0)
+                {
+                    buttonIndex = buttons.Count - 1;
+                }
+                currentButton = buttons[buttonIndex];
+                selectorObject.position = new Vector3(selectorObject.position.x, currentButton.transform.position.y);
+                
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (mainWindow.activeSelf)
+            {
+                buttonIndex++;
+                if (buttonIndex > buttons.Count - 1)
+                {
+                    buttonIndex = 0;
+                }
+                currentButton = buttons[buttonIndex];
+                selectorObject.position = new Vector3(selectorObject.position.x, currentButton.transform.position.y);
+                
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!mainWindow.activeSelf)
+            {
+                goBackButton.onClick.Invoke();
+                return;
+            }
+            currentButton.onClick.Invoke(); 
+        }
+    }
+
+    public void ButtonAssign(int num)
+    {
+        Enum aux = (ButtonSwitch) num;
+        switch (aux)
+        {
+           case ButtonSwitch.PlayButton:
+               print("Play"); // Cambiar a accion real
+               return;
+           case ButtonSwitch.HelpButton:
+               mainWindow.SetActive(false);
+               helpWindow.SetActive(true);
+               selectorObject.position = new Vector3(selectorObject.position.x, goBackButton.transform.position.y);
+               return;
+           case ButtonSwitch.CreditsButton:
+               mainWindow.SetActive(false);
+               creditsWindow.SetActive(true);
+               selectorObject.position = new Vector3(selectorObject.position.x, goBackButton.transform.position.y);
+               return;
+           case ButtonSwitch.QuitButton:
+               if (Application.isEditor)
+               {
+                   UnityEditor.EditorApplication.isPlaying = false;
+               }
+               Application.Quit();
+               return;
+           default: return;
+        } 
+    }
+
+    public void GoBackButtonHandler()
+    {
+        selectorObject.position = new Vector3(selectorObject.position.x, currentButton.transform.position.y);
+        mainWindow.SetActive(true);
+        helpWindow.SetActive(false);
+        creditsWindow.SetActive(false);
+    }
+}
