1a1a21bb91f55bd5d85c2f61eec644948064dfb0
DaxterGaston <daxtergaston@hotmail.com>
2021-05-13 01:45:06 -0300

    Se soluciona error por el cual el enemigo no encontraba correctamente al player.

--- Progra2/Assets/Scripts/Controllers/EnemyBehaviour.cs
+++ Progra2/Assets/Scripts/Controllers/EnemyBehaviour.cs
@@ -4,63 +4,67 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private LayerMask _playerLayer;
+    [SerializeField]
+    private LayerMask _viewObstacle;
+
     public float velocidad;
     public float DistanciaDeParado;
     public float DistanciaDeFueraDeVision;
 
     public float tiempoDeDisparo;
     public float tiempoInicialDeDisparo;
-    Collider2D[] colliders;
+    private Collider2D _collider;
     public GameObject bala;
+
+    private Vector3 _test;
 
     // Start is called before the first frame update
     void Start()
     {
         tiempoDeDisparo = tiempoInicialDeDisparo;
-        Physics2D.queriesStartInColliders = false;
-        
+        _test = transform.position;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        colliders = Physics2D.OverlapCircleAll(transform.position, DistanciaDeFueraDeVision);
-        foreach (var item in colliders)
+        _collider = Physics2D.OverlapCircle(transform.position, DistanciaDeFueraDeVision, _playerLayer);
+        if (_collider)
         {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, _collider.transform.position, Vector2.Distance(transform.position, _collider.transform.position), _viewObstacle);
+            Debug.DrawLine((Vector2)transform.position, (Vector2)_collider.transform.position, Color.magenta);
 
-            if (item.CompareTag("Player"))
+            if (!hit)
             {
-                RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, item.transform.position, DistanciaDeFueraDeVision, 10);
-                Debug.DrawRay((Vector2)transform.position, (Vector2)item.transform.position, Color.magenta);
-                if (hitInfo)
-                {
-                    if (Vector2.Distance(transform.position, item.transform.position) <= DistanciaDeFueraDeVision) { }
-                    {
-                        if (Vector2.Distance(transform.position, item.transform.position) > DistanciaDeParado)
-                        {
-
-                            if (hitInfo.collider.CompareTag("Player"))
-                                transform.position = Vector2.MoveTowards(transform.position, item.transform.position, velocidad * Time.deltaTime);
-
-                        }
-                        else if (Vector2.Distance(transform.position, item.transform.position) <= DistanciaDeParado)
-                        {
-
-                            if (hitInfo.collider.CompareTag("Player"))
-                                transform.position = transform.position;
-
-                        }
-                    }
-                }
-
+                //TODO: Hacer algo, aca ya esta el player en rango de vision para el enemigo, a una distancia que puede verlo y no hay ninguna pared en el medio.
             }
-        }
+            
+            //if (Vector2.Distance(transform.position, _collider.transform.position) <= DistanciaDeFueraDeVision) { }
+            //{
+            //    if (Vector2.Distance(transform.position, _collider.transform.position) > DistanciaDeParado)
+            //    {
 
+            //        if (hitInfo.collider.CompareTag("Player"))
+            //            transform.position = Vector2.MoveTowards(transform.position, _collider.transform.position, velocidad * Time.deltaTime);
+
+            //    }
+            //    else if (Vector2.Distance(transform.position, _collider.transform.position) <= DistanciaDeParado)
+            //    {
+
+            //        if (hitInfo.collider.CompareTag("Player"))
+            //            transform.position = transform.position;
+
+            //    }
+            //}
+        }
     }
 
     private void OnDrawGizmosSelected()
     {
-        
+        Gizmos.DrawWireSphere(transform.position, DistanciaDeFueraDeVision);
+        Gizmos.DrawLine((Vector2)transform.position, (Vector2)_test);
     }
 }
