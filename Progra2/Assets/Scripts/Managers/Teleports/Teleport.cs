94efe5d6bdf5677224267443cab11463d5dfda2f
DaxterGaston <daxtergaston@hotmail.com>
2021-07-07 16:54:18 -0300

    Colliders del mapa del nivel 1.

---
+++ Progra2/Assets/Scripts/Managers/Teleports/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField]
+    private int _id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Es collider");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Es Trigger");
+    }
+}
